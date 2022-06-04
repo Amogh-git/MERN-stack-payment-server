b32ab52609bcde9f06488889e0d1d32814e991cf
amoghkulkarni <amoghjkul21@gmail.com>
2022-06-04 17:28:31 +0530

    initial commit

---
+++ routes/payment.js
@@ -0,0 +1,69 @@
+const router = require("express").Router()
+const Razorpay = require("razorpay")
+const crypto = require("crypto")
+
+
+
+
+router.post("/orders",async(req,res)=>{
+    try{
+        const instance = new Razorpay({
+            key_id:process.env.KEY_ID,
+            key_secret:process.env.KEY_SECRET,
+
+        });
+
+
+        const options = {
+            amount: req.body.amount*100,
+            currency: "INR",
+            // reciept: crypto.randomBytes(10).toString('hex')
+        }
+
+
+        instance.orders.create(options, async(error, order)=>{
+            if(error){
+                console.log("error1 =>",error);
+                res.status(500).json({message:"Something went wrong!"});
+            }
+            else
+                res.status(200).json({data:order})
+        })
+    }
+    catch(error){
+        console.log("error2=>",error);
+        res.status(500).json({message:"Internal server error!"});
+    }
+})
+
+
+
+
+router.post("/verify", async(req,res)=>{
+    try{
+        const{
+            razorpay_order_id,
+            razorpay_payment_id,
+            razorpay_signature
+        } = req.body;
+        const sign = razorpay_order_id + "|" + razorpay_payment_id;
+        const expectedSign = crypto
+        .createHmac("sha256", process.env.KEY_SECRET)
+        .update(sign.toString())
+        .digest("hex")
+
+
+        if(razorpay_signature === expectedSign){
+            res.status(200).json({message:"payment verified successfully!"});
+        }
+        else{
+            res.status(400).json({message:"invalid signature sent!"});
+        }
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message:"Internal server error!"})
+    }
+})
+
+module.exports = router
