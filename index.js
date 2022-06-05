360bd154f18e9c7390b177dce39df4237e5837d9
amoghkulkarni <amoghjkul21@gmail.com>
2022-06-05 12:13:18 +0530

    commit Signed-off-by: amoghkulkarni <amoghjkul21@gmail.com>

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 const paymentRoutes = require("./routes/payment")
 const app = express()
-
+app.use(cors())
 
 
 dotenv.config()
 
 app.use(express.json())
-app.use(cors())
+
 app.options('*', cors()) 
 app.use("/api/payment",paymentRoutes)
 
